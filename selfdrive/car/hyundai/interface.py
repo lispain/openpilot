c7ffe21d5a9a96d7eb9b917f842f7919813e77f0
lispain <68838103+lispain@users.noreply.github.com>
2021-06-28 18:35:33 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -218,12 +218,12 @@ class CarInterface(CarInterfaceBase):
     ret.gasMaxBP = [0., 15. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS] 
     ret.gasMaxV = [1.5, 1.1, 0.6, 0.5, 0.4, 0.3]    # max gas allowed
     ret.brakeMaxBP = [0., 30. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS] 
-    ret.brakeMaxV = [0.7, 2.5, 0.5]   # max brake allowed
+    ret.brakeMaxV = [0.7, 3.0, 0.9]   # max brake allowed
 
     ret.longitudinalTuning.kpBP = [0., 15. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kpV = [0.59, 0.65, 0.52, 0.34, 0.31, 0.28]
     ret.longitudinalTuning.kiBP = [0., 15. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kiV = [0.015, 0.021, 0.020, 0.016, 0.015, 0.015]
+    ret.longitudinalTuning.kiV = [0.015, 0.023, 0.0215, 0.017, 0.015, 0.015]
 
     ret.longitudinalTuning.deadzoneBP = [0., 17.]
     ret.longitudinalTuning.deadzoneV = [0., 0.1]
