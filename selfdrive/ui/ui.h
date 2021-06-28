250854b9622ea4403de057a7251193ace315ac40
openpilotusers <>
2021-06-28 21:56:12 +0900

    openpilot v0.8.6

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -187,7 +187,7 @@ typedef struct UIScene {
   vertex_data lead_vertices[2];
 
   float light_sensor, accel_sensor, gyro_sensor;
-  bool started, ignition, is_metric, longitudinal_control, end_to_end, model_long;
+  bool started, ignition, is_metric, longitudinal_control, end_to_end;
   uint64_t started_frame;
 
 
