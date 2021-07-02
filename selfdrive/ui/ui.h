d2df40cdcd1f2871fa67fdbf6f26d6d02d1430ef
openpilotusers <>
2021-07-03 02:44:33 +0900

    openpilot v0.8.6

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -160,6 +160,7 @@ typedef struct UIScene {
   bool apks_enabled;
   bool is_OpenpilotViewEnabled;
   bool driving_record;
+  bool move_to_background;
 
   cereal::DeviceState::Reader deviceState;
   cereal::RadarState::LeadData::Reader lead_data[2];
