2e979460aca20b1dae19b471b3e49e4244145ca8
Vehicle Researcher <>
2021-07-07 17:18:22 +0900

    openpilot v0.8.6

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -328,7 +328,7 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : QWidget(parent) {
       fs_watch->addPath(paramsPath + "/d/UpdateFailedCount");
     }
     std::system("/data/openpilot/gitcommit.sh");
-    QTimer::singleShot(2000, []() {
+    QTimer::singleShot(1500, []() {
       QString desc = "";
       QString commit_local = QString::fromStdString(Params().get("GitCommit").substr(0, 10));
       QString commit_remote = QString::fromStdString(Params().get("GitCommitRemote").substr(0, 10));
