6adf435375670ca306bd408e9f29333fb6231505
Vehicle Researcher <>
2021-07-07 18:00:18 +0900

    openpilot v0.8.6

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -340,7 +340,7 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : QWidget(parent) {
       } else {
         desc += QString("업데이트가 있습니다. 아래 Git Pull에서 실행을 눌러 업데이트 하세요.");
       }
-      Params().put("LastUpdateTime", QDateTime::currentDateTime().toString(ISO::Date));
+      Params().put("LastUpdateTime", QDateTime::currentDateTime().toString(Qt::ISODate));
       if (ConfirmationDialog::confirm(desc)) {
       }
     });
