eb7e0b066ef3d03201d4a5bf923e8a1d21e159e4
Vehicle Researcher <>
2021-07-08 15:32:32 +0900

    openpilot v0.8.6

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -232,7 +232,6 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kfBP = [0., 4., 9., 17., 23., 31.]
     ret.longitudinalTuning.kfV = [1., 1.05, 1.05, 1., 1., 1.]
 
-    ret.enableCamera = True
     ret.enableBsm = 0x58b in fingerprint[0]
 
     ret.stoppingControl = True
