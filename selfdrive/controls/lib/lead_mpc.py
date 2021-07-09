50c770a728a5f79c20d201628ced627483178558
Vehicle Researcher <>
2021-07-09 18:31:36 +0900

    openpilot v0.8.6

--- selfdrive/controls/lib/lead_mpc.py
+++ selfdrive/controls/lib/lead_mpc.py
@@ -89,7 +89,7 @@ class LeadMpc():
 
     # neokii value, opkr mod
     cruise_gap = int(clip(CS.cruiseGapSet, 1., 4.))
-    dynamic_TR = interp(self.cur_state[0].v_ego*3.6, [0, 20, 40, 60, 110], [0.9, 1.2, 1.4, 1.65, 1.9] )
+    dynamic_TR = interp(v_ego*3.6, [0, 20, 40, 60, 110], [0.9, 1.2, 1.4, 1.65, 1.9] )
     TR = interp(float(cruise_gap), [1., 2., 3., 4.], [dynamic_TR, self.cruise_gap2, self.cruise_gap3, self.cruise_gap4])
 
 
