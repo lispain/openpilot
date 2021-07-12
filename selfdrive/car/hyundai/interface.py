a617a40be0ad956a67e4c1d366d8b4487fdd3fc3
openpilotusers <>
2021-07-13 08:00:31 +0900

    openpilot v0.8.6

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -18,7 +18,7 @@ def compute_gb_1(accel, speed):
 def compute_gb_2(accel, speed):
   creep_brake = 0.0
   creep_speed = 2.3
-  creep_brake_value = 0.15
+  creep_brake_value = 0.3
   if speed < creep_speed:
     creep_brake = (creep_speed - speed) / creep_speed * creep_brake_value
   return float(accel) / 3.0 - creep_brake
