d0ad5bd1d23e87582bb678064e2b6a98985d36d2
openpilotusers <>
2021-07-15 22:13:47 +0900

    openpilot v0.8.6

--- common/spinner.py
+++ common/spinner.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
   if PREBUILT:
     with Spinner() as s:
       # opkr
-      s.update("OPENPILOT\nQuick Booting...")
+      s.update("OPENPILOT Quick Booting...")
       time.sleep(7.0)
   else:
     with Spinner() as s:
