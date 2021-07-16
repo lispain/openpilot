4fa3684b12fccaca6b6e734656a9ded112cd2746
lispain <68838103+lispain@users.noreply.github.com>
2021-07-16 12:33:06 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -232,7 +232,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kdBP = [0., 15. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
     ret.longitudinalTuning.kdV = [0.7, 0.65, 0.5, 0.4, 0.3, 0.2]
     ret.longitudinalTuning.kfBP = [0., 15. * CV.KPH_TO_MS, 30. * CV.KPH_TO_MS, 60. * CV.KPH_TO_MS, 80. * CV.KPH_TO_MS, 110. * CV.KPH_TO_MS]
-    ret.longitudinalTuning.kfV = [1., 1., 1., 1., 1., 1.]
+    ret.longitudinalTuning.kfV = [1., 1., 1., 0.9, 0.85, 0.8]
 
     ret.enableBsm = 0x58b in fingerprint[0]
 
